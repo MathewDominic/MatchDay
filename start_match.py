5978b077327b14e3613fd0e5e9a60949d6ac555f
Mathew Dominic Puthoor <mathew@playsimple.in>
2018-09-17 02:45:36 +0530

    Adding script to automatically start live matches

--- start_match.py
+++ start_match.py
@@ -1,12 +1,13 @@
 import os
 import json
-import time
+import subprocess
+import requests
 import logging
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from config import constants
-from utils import initLogging
+from utils import init_logging
 
 API_KEY = constants['SPORTSMONK_API_KEY']
 cred = credentials.Certificate(os.path.expanduser('~/matchday-firebase-firebase-adminsdk-83hhc-40b0ae1594.json'))
@@ -16,6 +17,11 @@ db = firestore.Client()
 
 
 if __name__ == '__main__':
-    initLogging(logging.INFO, filename=os.path.expanduser('~/logs/start_match.log'))
-    matches = list(db.collection('matches')
-                   .where(u'timestamp', u'<', int(time.time()) * 1000).where(u'started', u'==', False))
+    init_logging(logging.INFO, filename=os.path.expanduser('~/logs/start_match.log'))
+    url = "https://soccer.sportmonks.com/api/v2.0/livescores/now?api_token=" + API_KEY
+    resp = requests.get(url)
+    matches = json.loads(resp.text)["data"]
+    for match in matches:
+        if match['league_id'] in constants['LEAGUES']:
+            cmd = "python {PATH}main.py {MATCH_ID} live".format(PATH=constants['ROOT_PATH'], MATCH_ID=match['id'])
+            subprocess.Popen(cmd, shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
