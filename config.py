10312ad466d44c3e2f09c29c600a506adacabd41
root <mathew.puthur@gmail.com>
2018-12-21 05:17:40 +0000

    config changes

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
 import yaml
 
-with open("/root/MatchDay/constants.yaml", "r") as f:
+with open(os.path.join(os.getcwdu(), "constants.yaml"), "r") as f:
     constants = yaml.load(f)
+
+with open(os.path.join(os.path.expanduser("~"), "tokens.yaml"), "r") as g:
+    constants.update(yaml.load(g))
+
